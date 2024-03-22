1b72f8d0cbce40f45bd8a44fec64d371cb5c6532
eutales <eu.tsiqueira@gmail.com>
2024-03-22 01:01:00 -0300

    Controle de Fluxo - Desafio https://github.com/digitalinnovationone/trilha-java-basico/tree/main/desafios/controle-fluxo

---
+++ src/Contador.java
@@ -0,0 +1,40 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+        System.out.print("Digite o primeiro número: ");
+        int parametroUm = terminal.nextInt();
+
+        System.out.print("Digite o segundo número: ");
+        int parametroDois = terminal.nextInt();
+
+        try {
+            contar(parametroUm, parametroDois);
+        } catch (ParametrosInvalidosException e) {
+            System.out.println("O parâmetro um é maior que o parâmetro dois");
+        }
+
+        terminal.close();
+    }
+
+    static void contar (int parametroUm, int paramtroDois) throws ParametrosInvalidosException {
+        if (parametroUm > paramtroDois){
+
+            throw new ParametrosInvalidosException();
+
+        }else{
+             int contagem = paramtroDois - parametroUm;
+             System.out.println("Será realizada a contagem de " + contagem + " números");
+             for (int i = 0; i < contagem; i++){
+
+                 System.out.println("Imprimindo número " + (i+1));
+
+             }
+        }
+
+
+    }
+
+}
